dd5cd811416ed1a605a0571b90828e1291539149
Ivan Dugalic <ivan.dugalic@axoniq.io>
2021-02-13 18:51:21 +0100

    PropagatingErrorHandler configured
    Errors on event processing side will be propagated to processor which will retry reading the event.

--- src/main/kotlin/com/fraktalio/restaurant/query/configuration/RestaurantQueryConfiguration.kt
+++ src/main/kotlin/com/fraktalio/restaurant/query/configuration/RestaurantQueryConfiguration.kt
@@ -1,6 +1,8 @@
 package com.fraktalio.restaurant.query.configuration
 
 import com.fraktalio.restaurant.web.configuration.Logging
+import org.axonframework.config.EventProcessingConfigurer
+import org.axonframework.eventhandling.PropagatingErrorHandler
 import org.axonframework.messaging.interceptors.LoggingInterceptor
 import org.axonframework.queryhandling.QueryBus
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,12 +12,25 @@ import org.springframework.context.annotation.Configuration
 internal class RestaurantQueryConfiguration :
     Logging {
 
-    /************************************************/
-    /* Register interceptors on the bus */
-    /************************************************/
+    /**************************************************/
+    /* Register handler interceptors on the query bus */
+    /**************************************************/
 
     @Autowired
     fun registerQueryInterceptorsOnBus(queryBus: QueryBus) {
         queryBus.registerHandlerInterceptor(LoggingInterceptor())
+    }
+
+    /*******************************************************************************************************************************/
+    /* Register default listener invocation handler on the event processors */
+    /* By default these exceptions are logged and processing continues with the next handler or message */
+    /* We change this behaviour by configuring custom listener PropagatingErrorHandler to re-throw the exception to the processor. */
+
+    /* https://docs.axoniq.io/reference-guide/axon-framework/events/event-processors#exceptions-raised-by-event-handler-methods */
+    /*******************************************************************************************************************************/
+
+    @Autowired
+    fun configureError(config: EventProcessingConfigurer) {
+        config.registerDefaultListenerInvocationErrorHandler { PropagatingErrorHandler.instance() }
     }
 }
